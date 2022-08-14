fdaa56daf75be8ada1a5066cb5c2a5bdb84fd3b7
yzn <yazannajjar44@gmail.com>
2022-08-14 19:41:52 +0300

    Added Personal Token To Increse The Limit For Github APi

--- api/repos.js
+++ api/repos.js
@@ -1,31 +1,45 @@
 import { END_POINT } from "../lib/constant";
 import axios from "axios";
 
+export const config = {
+  headers: {
+    "Content-type": "application/json",
+    "Authorization": `Bearer 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`, // Personal Access Token To Trigger Github Search API
+  },
+};
+
+const extractFilesExtension = (data) =>{
+  const filesExt = new Set(null);
+  data?.tree.filter(item => item.type === "blob").forEach(item => {
+
+    if(!item.path?.split(".")?.length) return;
+
+    //Check For The Length Of Extension Files
+    if(item.path?.split(".").slice(-1)[0]?.length < 10){
+      filesExt.add(item.path?.split(".")?.slice(-1)?.join(""));
+    }
+
+  });
+  return [...filesExt];
+};
+
 export const fetchRepos = async (username, pageNum = 1) => {
   if(username.length === 0) return;
-  const response = await axios.get(`${ END_POINT }/search/repositories?q=is:public+name:${ username }+forks:>=4&per_page=10&page=${pageNum}`);
+  const response = await axios.get(`${ END_POINT }/search/repositories?q=is:public+name:${ username }+forks:>=4&per_page=5&page=${pageNum}`, config);
   return response.data
 };
 
 export const fetchForkedUsers = async (repoName, OwnerName) => {
   if(repoName.length === 0) return;
 
-  const response = await axios.get(`${ END_POINT }/repos/${OwnerName}/${repoName}/forks`);
+  const response = await axios.get(`${ END_POINT }/repos/${OwnerName}/${repoName}/forks`, config);
   const findLastUsersForked = response.data.slice(0,3).map(item => ({name: item.owner.login, profileLink : item.html_url, repoName :item.name}));
   return findLastUsersForked;
 };
 
 export const fetchFilesExtension = async (repoName, ownerName) => {
   if(repoName.length === 0) return;
-  const response = await axios.get(`${ END_POINT }/repos/${ownerName}/${repoName}/git/trees/master?recursive=1`);
-  if(response?.data){
-    const filesExt = new Set(null);
-    response.data?.tree.filter(item => item.type === "blob").forEach(item => {
-      console.log(item.path?.split(".").slice(-1)[0],"item.path?.split(\".\").slice(-1)[0]");
-      if(item.path?.split(".").slice(-1)[0]?.length < 10 && item.path?.split(".")?.length > 1){
-       filesExt.add(item.path?.split(".")?.slice(-1)?.join(""));
-      }
-    });
-    return [...filesExt];
-  }
+  const response = await axios.get(`${ END_POINT }/repos/${ownerName}/${repoName}/git/trees/master?recursive=1`, config);
+
+  return extractFilesExtension(response.data)
 };
